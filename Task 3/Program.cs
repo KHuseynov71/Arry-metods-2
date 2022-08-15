e672e1feb56da4b30f08e97f313e0b2b82aa92df
unknown <kamranmh@code.edu.az>
2022-08-15 11:25:47 +0400

    metodlar elave etdim

--- Task 3/Program.cs
+++ Task 3/Program.cs
@@ -6,27 +6,28 @@ namespace Task_3
     {
         static void Main(string[] args)
         {
-            int[] nums = { 1, 3, 5, 7, -1, 3, 8 };
-            Change(nums);
-            foreach (int item in nums)
-            {
-                Console.WriteLine(item);
-            }
-        }
-        static void Change(int[] nums)
-        {
-            int min = nums[0];
-            int max = nums[0];
-            for (int i = 0; i < nums.Length; i++)
-            {
-                if (nums[min] > nums[i])
-                    min = i;
-                if (nums[max] < nums[i])
-                    max = i;
-            }
-            nums[min] = nums[min] + nums[max];
-            nums[max] = nums[min] - nums[max];
-            nums[min] = nums[min] - nums[max];
+            //    int[] nums = { 1, 3, 5, 7, -1, 3, 8 };
+            //    Change(nums);
+            //    foreach (int item in nums)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            //static void Change(int[] nums)
+            //{
+            //    int min = nums[0];
+            //    int max = nums[0];
+            //    for (int i = 0; i < nums.Length; i++)
+            //    {
+            //        if (nums[min] > nums[i])
+            //            min = i;
+            //        if (nums[max] < nums[i])
+            //            max = i;
+            //    }
+            //    nums[min] = nums[min] + nums[max];
+            //    nums[max] = nums[min] - nums[max];
+            //    nums[min] = nums[min] - nums[max];
+            Console.WriteLine("sxnkjasnxjknx\"xsnxjsnxj\"xjnjnxjs");
         }
     }
 }
